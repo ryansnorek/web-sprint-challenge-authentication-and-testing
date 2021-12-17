f473d5251808162b24b0d1ece7ada2e52fd2c612
Ryan <snorekr@gmail.com>
2021-12-17 10:27:58 -0500

    fixed: import jwt/secret

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config");
+
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) return next({ status: 401, message: "token required" });
