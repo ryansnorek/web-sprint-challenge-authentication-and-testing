257d7351e1bea405f0c78f761a2ec36541ef81ec
Ryan <snorekr@gmail.com>
2021-12-17 06:15:21 -0500

    check user doesnt exist middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -7,9 +7,11 @@ module.exports = {
   credentialsAreValid,
 };
 
-function userDoesNotExistAlready(req, res, next) {
-  console.log("userDoesNotExistAlready");
-  next();
+async function userDoesNotExistAlready(req, res, next) {
+  const { username } = req.body;
+  const error = { message: "username taken", status: 422 };
+  const user = await db("users").where({ username: username })
+  !user.length ? next() : res.json(error);
 }
 
 function hashThePassword(req, res, next) {
@@ -18,9 +20,9 @@ function hashThePassword(req, res, next) {
 }
 
 function reqBodyHasUsernameAndPassword(req, res, next) {
-    const { username, password } = req.body;
-    const error = { message: "username and password required" };
-    username && password ? next() : res.json(error);
+  const { username, password } = req.body;
+  const error = { message: "username and password required" };
+  username && password ? next() : res.json(error);
 }
 // 4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
 //       the response body should include a string exactly as follows: "invalid credentials".
