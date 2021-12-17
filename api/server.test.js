936e81d106fe4d43d4c8d86f44acfe31e2e7e411
Ryan <snorekr@gmail.com>
2021-12-17 11:11:45 -0500

    unit testing

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const db = require("../data/dbConfig");
 const server = require("./server");
-const { getAll, findById } = require("./models/users-model");
+const { find, add, findById } = require("./models/users-model");
 
 const user = { username: "bo_peep", password: 1 };
 
@@ -23,6 +23,18 @@ test("sanity", () => {
 test("correct env", () => {
   expect(process.env.NODE_ENV).toBe('testing')
 });
+
+describe("Unit testing", () => {
+  test("able to add user", async () => {
+    await add(user);
+    expect(find(user)).toBeTruthy()
+  })
+  test("able find user by id", async () => {
+    await add(user);
+    expect(findById(1)).not.toBe()
+  })
+})
+
 
 describe("[POST] /register", () => {
   test("successful register", async () => {
